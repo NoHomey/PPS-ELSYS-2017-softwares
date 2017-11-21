fe1103f5834b086e1b3476f0479b23fdd0a4f87e
Karina Kozarova <karinakozarova1303@gmail.com>
2017-11-21 21:46:01 +0200

    added second task

---
+++ A/12/homework2/task2.rb
@@ -0,0 +1,41 @@
+require 'csv'
+require 'matrix'
+
+matrix1 = "./matrix1.csv"
+matrix2 = "./matrix2.csv"
+
+line_count = 0
+elements_in_line_count = 0
+arr1 = Array.new
+arr2 = Array.new
+
+CSV.foreach(matrix1) do |row|
+  arr1 << row.map(&:to_i)
+  line_count += 1
+  elements_in_line_count =  row.size
+end
+
+n1 = elements_in_line_count
+m1 = line_count
+
+line_count = 0
+elements_in_line_count = 0
+
+CSV.foreach(matrix2) do |row|
+  arr2 << row.map(&:to_i)
+  line_count += 1
+  elements_in_line_count =  row.size
+end
+
+n2 = elements_in_line_count
+m2 = line_count
+begin
+  if m2 != n1
+    puts "undefined"
+  else
+    res = (Matrix.rows(arr1)*Matrix.rows(arr2)).symmetric?
+    puts !res
+  end
+rescue ExceptionForMatrix::ErrDimensionMismatch
+  puts "false"
+end
