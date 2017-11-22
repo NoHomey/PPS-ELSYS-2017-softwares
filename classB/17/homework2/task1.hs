3e29c15e39327ca89f973a177e7fff5abf548a87
mdatsev <martin.datsev.p@gmail.com>
2017-11-22 17:47:16 +0200

    some refactoring

--- classB/17/homework2/task1.hs
+++ classB/17/homework2/task1.hs
@@ -1,5 +1,5 @@
-import Data.List.Split --splitOn
-import Data.List --transpose
+import Data.List.Split (splitOn)
+import Data.List (transpose)
 
 main :: IO ()
 main = do
@@ -7,23 +7,24 @@ main = do
     csv2 <- readFile "matrix2.csv"
     let m1 = csvToMatrix csv1
     let m2 = csvToMatrix csv2
-    let result = matrixAdd m1 m2
+    let result = addMatrix m1 m2
     putStrLn $  if sameSize m1 m2 then
-                    if negativeMatrix result == transpose result 
+                    if negateMatrix result == transpose result 
                         then "true"
                         else "false"
                 else "undefined"
 
 sameSize :: Num a => [[a]] -> [[a]] -> Bool
-sameSize m1 m2 = length m1 == length m2 && length (head m1) == length (head m2)
+sameSize m1 m2 = length m1 == length m2 &&
+                    map length m1 == map length m2
 
-matrixAdd :: Num a => [[a]] -> [[a]] -> [[a]]
-matrixAdd = zipWith $ zipWith (+)
+addMatrix :: Num a => [[a]] -> [[a]] -> [[a]]
+addMatrix = zipWith $ zipWith (+)
 
-negativeMatrix :: Num a => [[a]] -> [[a]]
-negativeMatrix = map $ map (0-)
+negateMatrix :: Num a => [[a]] -> [[a]]
+negateMatrix = map $ map negate
 
 csvToMatrix :: String -> [[Double]]
-csvToMatrix = (map (map (\x -> read x :: Double) -- string to double
-                    . (splitOn ","))) -- split values
-                . (splitOn "\n") -- split lines
+csvToMatrix = (map (map read -- read strings to doubles
+                        . (splitOn ","))) -- split values
+                            . lines -- split lines
--- classB/17/homework2/task2.hs
+++ classB/17/homework2/task2.hs
@@ -1,5 +1,5 @@
-import Data.List.Split --splitOn
-import Data.List --transpose
+import Data.List.Split (splitOn)
+import Data.List (transpose)
 
 main :: IO ()
 main = do
@@ -7,7 +7,7 @@ main = do
     csv2 <- readFile "matrix2.csv"
     let m1 = csvToMatrix csv1
     let m2 = csvToMatrix csv2
-    let result = matrixMultiply m1 m2
+    let result = multiplyMatrix m1 m2
     putStrLn $  if canMultiply m1 m2 then
                     if result == transpose result 
                         then "true"
@@ -17,10 +17,14 @@ main = do
 canMultiply :: Num a => [[a]] -> [[a]] -> Bool
 canMultiply m1 m2 = length (head m1) == length m2
 
-matrixMultiply :: Num a => [[a]] -> [[a]] -> [[a]]
-matrixMultiply m1 m2 = map (\m1row -> map (sum . zipWith (*) m1row) (Data.List.transpose m2)) m1
+multiplyMatrix :: Num a => [[a]] -> [[a]] -> [[a]]
+multiplyMatrix m1 m2 = map (\m1row -> map (sum --sum the products
+                                            . zipWith (*) --muliply all pairs
+                                                m1row) --m1 rows
+                                                    (transpose m2)) --transposed m2 rows ( m2 cols )
+                                                        m1 --map over m1 rows
 
 csvToMatrix :: String -> [[Double]]
-csvToMatrix = (map (map (\x -> read x :: Double) -- string to double
-                    . (splitOn ","))) -- split values
-                . (splitOn "\n") -- split lines
+csvToMatrix = (map (map read -- read strings to doubles
+                        . (splitOn ","))) -- split values
+                            . lines -- split lines
