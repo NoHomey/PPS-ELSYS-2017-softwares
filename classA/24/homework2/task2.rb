8d2a2dc6421d3b35baf63cee69bd701d782791df
stefan50 <ste.angelov.tues19@gmail.com>
2017-11-22 18:34:00 +0200

    Done homework

--- classA/24/homework2/task2.rb
+++ classA/24/homework2/task2.rb
@@ -1,7 +1,6 @@
 require 'csv' #this is a gem
 require 'matrix'
 
-
 matrix1 = File.new(ARGV[0])
 matrix2 = File.new(ARGV[1])
 
@@ -11,14 +10,23 @@ mat2 = CSV.read(matrix2)
 mat1 = Matrix[*mat1].map {|el| el.to_i} #elements of the matrix becoming integers
 mat2 = Matrix[*mat2].map {|el| el.to_i} #elements of the matrix becoming integers
 
-if mat1.row_count == mat2.row_count and mat1.column_count == mat2.column_count
-	mat3 = mat1 + mat2
-	if mat3.symmetric?
-		puts 'true'
-	else
-		puts 'false'
+if mat1.column_count == mat2.row_count
+	mat3 = mat1 * mat2
+	if mat3.square?
+		mat4 = mat3.t                   #transposing mat3
+		mat4 = mat4.map {|el| el = -el} #At = -At
+		if mat4.eql?(mat3)              #-At == A
+			puts 'true'
+		else
+			puts 'false'
+		end
 	end
 else
 	puts 'undefined'
 end
+
+
+
+
+
 
