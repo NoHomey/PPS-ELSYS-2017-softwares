897f3cd118dd85e1724b1beb5b6accd15f7ce965
wencakisa <wencakisa@gmail.com>
2017-11-23 21:28:04 +0200

    Supporting argumentless

--- classA/4/homework2/task2.py
+++ classA/4/homework2/task2.py
@@ -4,11 +4,13 @@ from matrix import extract_matrix_from_csv, Matrix
 
 
 def main():
-    first_matrix_filename = sys.argv[1]
+    has_provided_filenames = len(sys.argv) > 2
+
+    first_matrix_filename = sys.argv[1] if has_provided_filenames else './matrix1.csv'
     first_matrix_values = extract_matrix_from_csv(first_matrix_filename)
     first_matrix = Matrix(first_matrix_values)
 
-    second_matrix_filename = sys.argv[2]
+    second_matrix_filename = sys.argv[2] if has_provided_filenames else './matrix2.csv'
     second_matrix_values = extract_matrix_from_csv(second_matrix_filename)
     second_matrix = Matrix(second_matrix_values)
     
