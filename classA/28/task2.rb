947a07f052176c09b5c118c7b659c678e5119bee
Dani-Ray <daniel_raykov@abv.bg>
2017-11-23 23:40:39 +0200

    Uploading hw2

---
+++ classA/28/task2.rb
@@ -0,0 +1,20 @@
+require 'matrix'
+require 'csv'
+
+m1 = Matrix[File.read("matrix1.csv").split(",")]
+m2 = Matrix[File.read("matrix2.csv").split(",")]
+m3 = Matrix.empty(0, 0)
+
+begin #try
+	m3 = m1 * m2
+
+		begin
+			if m3 == m3.transpose() * (-1)
+				puts "true"
+			end
+		rescue
+			puts "false"
+		end
+rescue #catch, if * operation or .transpose() is unsuccsessful it throws ErrDimensionMismatch exception
+	puts "undefined"
+end
