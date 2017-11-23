ffb919f061883ac7e7be35935a4d9e7601ac2e9c
RobertaNetzova123 <roberta.netzova@fmail.com>
2017-11-23 12:46:18 -0800

    task1 and task2

---
+++ classB/21/homework2/task1.rb
@@ -0,0 +1,33 @@
+require 'csv'
+require 'matrix'
+	arr1 = Array.new
+	arr1_rows = 0
+	arr1_col = 0
+	arr2 = Array.new
+	arr2_rows = 0
+	arr2_col = 0
+	CSV.foreach ('matrix1.csv') do |row| 
+		arr1 << row.map(&:to_i)
+		arr1_rows += 1
+		arr1_col = row.size
+	end
+
+	CSV.foreach ('matrix2.csv') do |row| 
+		arr2 << row.map(&:to_i)
+		arr2_rows += 1
+		arr2_col = row.size
+	end
+
+	begin
+		if arr1_rows != arr2_rows || arr1_col != arr2_col
+			puts "undefined"
+		else 
+			  (Matrix.rows(arr1) + Matrix.rows(arr2)).symmetric?
+				puts "false"
+			  end
+			  rescue ExceptionForMatrix::ErrDimensionMismatch
+				puts "true"
+		end
+	
+
+		
